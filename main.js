c26021bf0064b7dad8707f1856ca41a823f84478
larmflower <lnarosetta@gmail.com>
2017-04-25 12:33:07 +0100

    Adding Arrays

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
- console.log("Up and running!");
+//  console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-  console.log("User flipped " + cardOne);
-  console.log("User flipped " + cardThree);
+//   console.log("User flipped " + cardOne);
+//   console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay= [];
+
+var cardOne= cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+
+var cardTwo= cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped King");
+

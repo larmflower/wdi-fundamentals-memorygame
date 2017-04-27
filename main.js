fdddb67cbcc923d4673c7d755edc3269320ecdce
larmflower <lnarosetta@gmail.com>
2017-04-27 09:42:02 +0100

    Updates

--- main.js
+++ main.js
@@ -7,27 +7,32 @@
 
 //   console.log("User flipped " + cardOne);
 //   console.log("User flipped " + cardThree);
+
+// Create array containing four objects and store in cards variable
 var cards = 
 [{
 	rank: "queen",
 	suit: "hearts",
 	cardImage: "queen-of-hearts.png"
 
-},{
+},
+{
 	rank: "queen",
 	suit: "diamonds",
 	cardImage: "queen-of-diamonds.png"
-},{
+},
+{
 	rank: "king",
 	suit: "hearts",
 	cardImage: "king-of-hearts.png"
-},{
+},
+{
 	rank: "king",
 	suit: "diamonds",
 	cardImage: "king-of-diamonds.png"
 }];
 
-
+// Create an empty array and store it in cardsInPlay variable
 var cardsInPlay= [];
 
 var checkForMath = function(){
@@ -37,26 +42,51 @@ var checkForMath = function(){
       console.log("Sorry, try again.");
   }
 
-}
+};
 
-var flipCard = function(cardId){
+// Create function that takes one argument cardId and store it in flipCard variable
+// create a for loop within the function that loops through the cards when two are displayed
+// Create else if statement in for loop to action when two showing cards match
+// Create alert when cards match to pop up in browser saying "you've found match"
+var flipCard = function(){ 
+var cardId = this.getAttribute('data-id');
 
-if(cardsInPlay.length === 2){
-} else if(cardsInPlay === 0){
-alert("You found a match!")
-}
+//  use the setAttribute method to update the src 
+//  attribute to the image of the card that was 
+//  just clicked, which is stored in the cardImage
+//   property in that card's object in the cards
+//   array.
+this.setAttribute('src', cards[cardId].cardImage);
 
+if(cardsInPlay.length % 2 === 0){
+} else if(cardsInPlay % 0 === 0){
+alert("You found a match!");
+};
 else {
- alert("Sorry, try again")
-}
+ alert("Sorry, try again");
+};
 console.log("User flipped " + cards[cardId].rank);
-}
+};
 
 cardsInPlay.push(cards[cardId].rank);
 
-flipCard(0);
-flipCard(2);
+// !!- Finish function below
+// Create function that takes no parameter or argument, store in createBoard variable
+// Create for loop within function that runs through each card and creates new one
+var createBoard = function(){
+for (var i=0; i< cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'back.png');
+cardElement.setAttribute('data-id', '[i]');
+document.getElementById('cardElement')[0].addEventListener('click', addEvent);
+document.getElementById('gameboard')[0].appendChild(boxElement);
+}
+};
+
+createBoard();
 
 console.log(cardImage);
 console.log(suit);
+
+
 
